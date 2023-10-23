cf442209f3a33bcc6e06bbeed9094adde3977695
Chr1sCG <112557454+Chr1sCG@users.noreply.github.com>
2023-10-23 19:41:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -23,24 +23,24 @@ const appConfig = require(`./config.app.json`);
 app.get(`/`, (req, res) => res.json(appConfig));
 
 app.post(`/validate`, wrap(async (req, res) => {  
-    const token = req.body.fields.token;
+    const token = req.body.fields.wstoken;
     
     if (token != null) {
-        const options = { headers: { 'Authorization': 'Token ' + wstoken } };
-        let response = await got('https://acme.fibery.io/api/commands', options);
+        const options = { headers: { 'Authorization': 'Token ' + token } };
+        let response = await got('https://readwise.io/api/v2/auth/', options);    // add 401 handling code here
 
-        if (response.statusCode === 200) {
-            if (req.body.fields.wsname) {
+        if (response.statusCode === 204) {
+            if (req.body.fields.connectionname) {
                 return res.json({
-                    name: `${req.body.fields.wsname}`
+                    name: `${req.body.fields.connectionname}`
                 });                    
             }
             return res.json({
-                name: 'Schema'
+                name: 'Readwise'
             });
         }
     }
-    res.status(401).json({message: `Invalid access token`});    
+    //res.status(401).json({message: `Invalid access token`});    
 }));
 
 const syncConfig = require(`./config.sync.json`);
@@ -50,20 +50,15 @@ const schema = require(`./schema.json`);
 app.post(`/api/v1/synchronizer/schema`, (req, res) => res.json(schema));
 
 app.post(`/api/v1/synchronizer/data`, wrap(async (req, res) => {
-
-    /*
+    
     let {requestedType, pagination, account, lastSynchronizedAt, filter} = req.body;
     const options = { headers: { 'Authorization': 'Token ' + account.token } };
-    */
         
-    if (requestedType !== `space` && requestedType != `database`) {
-        throw new Error(`Only these items can be synchronized`);
+    if (requestedType !== `database` && requestedType != `space`) {
+        throw new Error(`Only these databases can be synchronized`);
     }
-
-    
-    
-    if (requestedType == `space`){
-        /*
+        
+    if (requestedType == `database`){
         var url = 'https://readwise.io/api/v2/highlights';
         let response = await got(url, options);
         let body = JSON.parse(response.body);
@@ -76,15 +71,12 @@ app.post(`/api/v1/synchronizer/data`, wrap(async (req, res) => {
             next = body.next;
             highlights = highlights.concat(body.results);
         }
-        */
         
-        //let items = highlights.map((h) => ({...h, id: uuid((h.id).toString()), name: (h.text.length > 100 ? h.text.slice(0,97) + "..." : h.text), text: h.text, book: uuid((h.book_id).toString()), tags: (h.tags).map((t) => t.name)}));
-        let items = [{id: uuid("1234"), name: "DummySpace"}];
+        let items = highlights.map((h) => ({...h, id: uuid((h.id).toString()), name: (h.text.length > 100 ? h.text.slice(0,97) + "..." : h.text), text: h.text, book: uuid((h.book_id).toString()), tags: (h.tags).map((t) => t.name)}));
         return res.json({items});
     }
     
-    else if (requestedType == `database`){
-        /*
+    else if (requestedType == `space`){
         var url = 'https://readwise.io/api/v2/books';
         let response = await got(url, options);
         let body = JSON.parse(response.body);
@@ -96,11 +88,9 @@ app.post(`/api/v1/synchronizer/data`, wrap(async (req, res) => {
             body = JSON.parse(response.body);
             next = body.next;
             books = books.concat(body.results);
-        }
-        */
-
-        let items = [{id: uuid("5678"), name: "DummyDatabase", space: uuid("1234")}];
-        //let items = books.map((b) => ({...b, id: uuid((b.id).toString()), name: b.title, tags: (b.tags).map((t) => t.name)}));
+        } 
+        
+        let items = books.map((b) => ({...b, id: uuid((b.id).toString()), name: b.title, tags: (b.tags).map((t) => t.name)}));
         return res.json({items});
     }
 }));
